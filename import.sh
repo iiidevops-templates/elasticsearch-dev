b710f222682455690e486b8ce5eac883f30dd8b1
jonathan <tryweb@ichiayi.com>
2021-09-06 15:37:33 +0800

    debug

--- import.sh
+++ import.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# Ref - https://github.com/elasticsearch-dump/elasticsearch-dump
 elasticdump --input "csv://data/imports.csv" --output=http://10.20.0.96:32306/my_index --type=data --csvDelimiter ";"
