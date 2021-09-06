59929301326560995fddd36a014e7a3ea2b22946
jonathan <tryweb@ichiayi.com>
2021-09-06 23:48:48 +0800

    修正以變數方式來呈現 service name

--- import.sh
+++ import.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Ref - https://github.com/elasticsearch-dump/elasticsearch-dump
 #elasticdump --input http://mysource.elasticsearch.net:9200/my_index --output=http://elasticsearch-dev-master-db-svc:9200/my_index --type=data
-elasticdump --input "csv:///mnt/data/imports.csv" --output=http://elasticsearch-dev-master-db-svc:9200/my_index --type=data --csvDelimiter ";"
+echo $1
+echo $2
+elasticdump --input "csv:///mnt/data/imports.csv" --output=http://$1-$2-db-svc:9200/my_index --type=data --csvDelimiter ";"
