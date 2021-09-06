aba00afa5d65d519cc2381f8fafdbff7bc762917
jonathan <tryweb@ichiayi.com>
2021-09-06 17:24:55 +0800

    debug

--- import.sh
+++ import.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Ref - https://github.com/elasticsearch-dump/elasticsearch-dump
 #elasticdump --input "csv:///mnt/data/imports.csv" --output=http://elasticsearch-dev-master-db-svc:9200/my_index --type=data --csvDelimiter ";"
-elasticdump --input "csv:///mnt/data/imports.csv" --output=http://10.20.0.96:32306/my_index --type=data --csvDelimiter ";"
+elasticdump --input "csv:///./data/imports.csv" --output=http://10.20.0.96:32306/my_index --type=data --csvDelimiter ";"
