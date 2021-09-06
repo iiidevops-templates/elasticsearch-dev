d02c25a725ad2436807544f9ecdfc98e981c2ed3
jonathan <tryweb@ichiayi.com>
2021-09-07 00:02:44 +0800

    調正 import 的顯示訊息

--- import.sh
+++ import.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Ref - https://github.com/elasticsearch-dump/elasticsearch-dump
-#elasticdump --input http://mysource.elasticsearch.net:9200/my_index --output=http://elasticsearch-dev-master-db-svc:9200/my_index --type=data
-echo $1
-echo $2
+echo
+echo "URL-http://$1-$2-db-svc:9200"
+echo
 elasticdump --input "csv:///mnt/data/imports.csv" --output=http://$1-$2-db-svc:9200/my_index --type=data --csvDelimiter ";"
+#elasticdump --input http://mysource.elasticsearch.net:9200/my_index --output=http://$1-$2-db-svc:9200/my_index --type=data
